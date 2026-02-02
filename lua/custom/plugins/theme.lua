2db63065d98405d8f2edbf20112a74f4c84b11cc
ghost <lan3r@proton.me>
2026-02-02 13:15:03 -0300

    rosepine modifications

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,7 +1,13 @@
+---@diagnostic disable: undefined-global
 return {
   'rose-pine/neovim',
   name = 'rose-pine',
   config = function()
+    require('rose-pine').setup {
+      styles = {
+        italic = false,
+      },
+    }
     vim.cmd 'colorscheme rose-pine'
   end,
 }
