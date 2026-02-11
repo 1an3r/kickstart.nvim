0a49b92cf1ead2d9c7f20ddc4c4b227916eccf5a
ghost <lan3r@proton.me>
2026-02-11 00:01:45 -0300

    changes keybinds.lua[

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -1,3 +1,6 @@
+---@diagnostic disable: undefined-global
 return {
   vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save File' }),
+  vim.keymap.set('n', '<leader>p', ':pu<CR>', { desc = 'Paste on line below' }),
+  vim.keymap.set('n', '<leader>P', ':pu!<CR>', { desc = 'Paste on line above' }),
 }
