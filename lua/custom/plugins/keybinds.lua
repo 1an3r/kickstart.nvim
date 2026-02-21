4bd3d89e96c89cbdd31cd408b64faa22ac9dddc6
1an3r <lan3r@proton.me>
2026-02-21 08:38:52 -0300

    idk what im doing

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -1,6 +1,12 @@
 ---@diagnostic disable: undefined-global
-return {
-  vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save File' }),
-  vim.keymap.set('n', '<leader>p', ':pu<CR>', { desc = 'Paste on line below' }),
-  vim.keymap.set('n', '<leader>P', ':pu!<CR>', { desc = 'Paste on line above' }),
-}
+
+vim.keymap.set('n', '<leader>dm', [[<cmd>%s/\r$//e<cr>]], { desc = 'Strip Windows ^M' })
+vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save File' })
+vim.keymap.set('n', '<leader>p', ':pu<CR>', { desc = 'Paste on line below' })
+vim.keymap.set('n', '<leader>P', ':pu!<CR>', { desc = 'Paste on line above' })
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.fileformats = 'unix,dos'
+return {}
